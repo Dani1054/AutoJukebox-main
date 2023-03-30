1a9a4e181950044a5e553b1b5f866ee62b7f175c
Dani1054 <80718811+Dani1054@users.noreply.github.com>
2023-03-30 23:19:34 +0200

    popravljanje bugova i dodavanje error messaga kada nema playliste

--- scripts/newSongsHandler.js
+++ scripts/newSongsHandler.js
@@ -1,29 +1,58 @@
 let nameOfPlaylistOpen;
 let numOfGenres;
-function checkForFocusElementGenres(){
-    
+let counterForOpening = 0;
+function checkForFocusElementGenres() {
+
     numOfGenres = document.querySelectorAll('.playlist-box');
     let inFocusElement = document.querySelector('#focus');
-    for(let i = 0; i<numOfGenres.length; i++){
-        if(numOfGenres[i]==inFocusElement){
+    for (let i = 0; i < numOfGenres.length; i++) {
+        if (numOfGenres[i] == inFocusElement) {
             //console.log(i);
             nameOfPlaylistOpen = document.getElementsByClassName("playlist-item")[i].textContent;
         }
     }
-    
+
 }
-
-
+let boolForCheckingError = false;
+let numOfPlaylists;
+function checkForNumberOfplaylists() {
+    let nodeOfAllPlaylists = document.querySelectorAll('.playlist-box');
+    numOfPlaylists = nodeOfAllPlaylists.length;
+}
 function closeSongsContainer() {
     document.getElementsByClassName("add-songs-container-shown")[0].className = "add-songs-container";
 }
 function openSongsContainer() {
     document.getElementsByClassName("add-songs-container")[0].className = "add-songs-container-shown";
+    checkForNumberOfplaylists();
+    if (numOfPlaylists > 0 && ((counterForOpening ==  0 && boolForCheckingError==false) || (counterForOpening ==  1 && boolForCheckingError==true))) {
+        createChekers();
+        checkButtonForAddingSongs();
+        counterForOpening++;
+
+    } else if(counterForOpening==0){
+        createErrorMessageInSongsAdder();
+        counterForOpening++;
+        boolForCheckingError = true;
+    }
+    
 }
 
-document.getElementsByClassName("button-for-adding-songs")[0].addEventListener("click", openSongsContainer);
-document.getElementsByClassName("close-songs")[0].addEventListener("click", closeSongsContainer);
+console.log(document.getElementsByClassName("add-songs-container")[0]);
 
+
+
+document.getElementsByClassName("button-for-adding-songs")[0].addEventListener("click", function(){
+    let elementForAddingSongsForChecking = document.getElementsByClassName("add-songs-container")[0];
+    console.log(elementForAddingSongsForChecking);
+    if(typeof elementForAddingSongsForChecking == "undefined"){
+        closeSongsContainer();
+    }else{
+        openSongsContainer();
+    }
+});
+document.getElementsByClassName("close-songs")[0].addEventListener("click", closeSongsContainer);
+if(document.getElementsByClassName(""))
 
 function createChekers() {
     for (let i = 0; i < songDatabase.length; i++) {
@@ -43,41 +72,60 @@ function createChekers() {
         newDivElement.appendChild(newInputElement);
         newDivElement.appendChild(newSpanElement);
     }
+    removeErrorMessageInSongsAdder();
     let buttonDiv = document.createElement("div");
     buttonDiv.className = "button-checklist";
 
     let textInButtonDiv = document.createElement("span");
     textInButtonDiv.textContent = "Spremi promjene";
 
-    let parentElementButton = document.getElementsByClassName("add-songs-container")[0];
+    let parentElementButton = document.getElementsByClassName("add-songs-container-shown")[0];
     parentElementButton.appendChild(buttonDiv);
     buttonDiv.appendChild(textInButtonDiv);
 };
 
-createChekers();
-let buttonDiv = document.getElementsByClassName("button-checklist")[0];
-buttonDiv.addEventListener("click", function (e) {
-    for (let i = 0; i < songDatabase.length; i++) {
-        let checkingCheckCurrentCheck = document.getElementsByClassName("check")[i];
 
-        if (checkingCheckCurrentCheck.checked == true) {
 
-            playlist0.push(songDatabase[i]);
-            checkForFocusElementGenres();
-            //console.log(nameOfPlaylistOpen);
-            songDatabase[i].playlist.push(nameOfPlaylistOpen);
-            //console.log(songDatabase[i].playlist);
 
-            playingNow = i;
+function createErrorMessageInSongsAdder() {
+    let newSpanElementForError = document.createElement("span");
+    newSpanElementForError.textContent = "Ne mogu se nadodati pjesme ako nisu u playlisti";
+    newSpanElementForError.className = "span-for-error";
+    let parentElementToErrorMessage = document.getElementsByClassName("add-songs-container-shown")[0];
+    parentElementToErrorMessage.appendChild(newSpanElementForError);
+}
 
-            createSongsInDragAndDrop(i);
-            changeTitle();
-            checkIfSongEnded();
-            playingElementCheck();
-            uncheckCheckers(i);
+function removeErrorMessageInSongsAdder() {
+    let spanForRemoval = document.getElementsByClassName("span-for-error")[0];
+    spanForRemoval.remove();
+}
+
+function checkButtonForAddingSongs() {
+    let buttonDiv = document.getElementsByClassName("button-checklist")[0];
+    buttonDiv.addEventListener("click", function (e) {
+        for (let i = 0; i < songDatabase.length; i++) {
+            let checkingCheckCurrentCheck = document.getElementsByClassName("check")[i];
+
+            if (checkingCheckCurrentCheck.checked == true) {
+
+                playlist0.push(songDatabase[i]);
+                checkForFocusElementGenres();
+                //console.log(nameOfPlaylistOpen);
+                songDatabase[i].playlist.push(nameOfPlaylistOpen);
+                //console.log(songDatabase[i].playlist);
+
+                playingNow = i;
+
+                createSongsInDragAndDrop(i);
+                changeTitle();
+                checkIfSongEnded();
+                playingElementCheck();
+                uncheckCheckers(i);
+            }
         }
-    }
-});
+    });
+}
+
 
 function uncheckCheckers(i) {
     let checkingCheckCurrentCheck = document.getElementsByClassName("check")[i];
@@ -112,5 +160,5 @@ function createSongsInDragAndDrop(i) {
 }
 
 
-document.getElementsByClassName("button-checklist")[0].addEventListener("click", checkSongsDown);
+//document.getElementsByClassName("button-checklist")[0].addEventListener("click", checkSongsDown);
 //setInterval(checkSongsDown, 1);
